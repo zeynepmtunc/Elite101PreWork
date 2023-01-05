8deabd51561deae7e313c0054cee1c2580953fe2
Zeynep <121909811+zeynepmtunc@users.noreply.github.com>
2023-01-05 01:36:56 +0000

    Elite101

---
+++ main.py
@@ -0,0 +1,21 @@
+Welcome= input("Hello there! How may I be of assistance?")
+
+
+print("Our store hours are from 8am - 5pm, Monday through Sunday")
+
+
+Question1= input("What else can I help you with? ")
+
+print("Our recent location is near Round Rock, Texas")
+
+Question2= input("Was there any other question you needed assistance with?")
+
+print("Our available products are natural makeup products all for less than $10")
+
+# print("")
+
+# print("Welcome, the answer to your question is" + Question1 + "our store hours are from 8 am - 5pm Monday through Sunday")
+
+# print("")
+
+
